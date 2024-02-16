ef1d3d01d1d01256cba6c44de7588fa65b06ea6a
ninathedev <realguybackup@gmail.com>
2024-02-16 18:52:37 +0800

    Initial commit

---
+++ src/main/java/com/github/ninathedev/place/Place.java
@@ -0,0 +1,21 @@
+package com.github.ninathedev.place;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Place extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("[WELCOME] Welcome to place!");
+        getLogger().info("[WELCOME] This plugin expects that all players are in creative mode and");
+        getLogger().info("[WELCOME] that the rest of the entire server is not accessible by people.");
+        getLogger().info("[WELCOME] Please make sure that nothing is letting the players roam out of the 3D canvas!");
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

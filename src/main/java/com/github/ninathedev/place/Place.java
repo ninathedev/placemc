eff003747469d70c379c4d04e1385a88e710036c
ninathedev <realguybackup@gmail.com>
2024-02-16 19:16:02 +0800

    hi

--- src/main/java/com/github/ninathedev/place/Place.java
+++ src/main/java/com/github/ninathedev/place/Place.java
@@ -6,6 +6,7 @@ public final class Place extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        this.saveDefaultConfig();
         getLogger().info("[WELCOME] Welcome to place!");
         getLogger().info("[WELCOME] This plugin expects that all players are in creative mode and");
         getLogger().info("[WELCOME] that the rest of the entire server is not accessible by people.");

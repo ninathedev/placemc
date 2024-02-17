c7e7f033bf7717a694691c31a21ccfd49a8301bf
ninathedev <realguybackup@gmail.com>
2024-02-17 10:55:23 +0800

    onplayerjoin added

--- src/main/java/com/github/ninathedev/place/Place.java
+++ src/main/java/com/github/ninathedev/place/Place.java
@@ -1,12 +1,18 @@
 package com.github.ninathedev.place;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Place extends JavaPlugin {
+public final class Place extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         this.saveDefaultConfig();
+        getServer().getPluginManager().registerEvents(this, this);
         getLogger().info("[WELCOME] Welcome to place!");
         getLogger().info("[WELCOME] This plugin expects that all players are in creative mode and");
         getLogger().info("[WELCOME] that the rest of the entire server is not accessible by people.");
@@ -18,5 +24,14 @@ public final class Place extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onPlayerJoinEvent(PlayerJoinEvent event) {
+        for (Player player1 : Bukkit.getOnlinePlayers()) {
+            for (Player player2 : Bukkit.getOnlinePlayers()) {
+                if (player1 != player2) player1.hidePlayer(player2);
+            }
+        }
     }
 }
--- src/main/java/com/github/ninathedev/place/ReloadConfig.java
+++ src/main/java/com/github/ninathedev/place/ReloadConfig.java
@@ -1,10 +1,12 @@
 package com.github.ninathedev.place;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Objects;
 import java.util.concurrent.ExecutionException;
 
 public class ReloadConfig implements CommandExecutor {
@@ -12,13 +14,7 @@ public class ReloadConfig implements CommandExecutor {
     // This method is called, when somebody uses our command
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        try {
-            Place.plugin.reloadConfig();
-            return true;
-        } catch (ExecutionException err) {
-            err.printStackTrace();
-            return false;
-        }
-
+        Objects.requireNonNull(Bukkit.getPluginManager().getPlugin("place")).reloadConfig();
+        return true;
     }
 }

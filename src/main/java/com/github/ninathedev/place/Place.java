0866cd3e0c2ed2b76915483931eb80734540279e
ninathedev <realguybackup@gmail.com>
2024-02-17 12:08:25 +0800

    registered cmd

--- src/main/java/com/github/ninathedev/place/Place.java
+++ src/main/java/com/github/ninathedev/place/Place.java
@@ -1,11 +1,18 @@
 package com.github.ninathedev.place;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.FallingBlock;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
 
 public final class Place extends JavaPlugin implements Listener {
 
@@ -13,6 +20,7 @@ public final class Place extends JavaPlugin implements Listener {
     public void onEnable() {
         this.saveDefaultConfig();
         getServer().getPluginManager().registerEvents(this, this);
+        Objects.requireNonNull(this.getCommand("reloadconfig")).setExecutor(new ReloadConfig());
         getLogger().info("[WELCOME] Welcome to place!");
         getLogger().info("[WELCOME] This plugin expects that all players are in creative mode and");
         getLogger().info("[WELCOME] that the rest of the entire server is not accessible by people.");
@@ -32,6 +40,20 @@ public final class Place extends JavaPlugin implements Listener {
             for (Player player2 : Bukkit.getOnlinePlayers()) {
                 if (player1 != player2) player1.hidePlayer(player2);
             }
+        }
+    }
+
+    @EventHandler
+    public void onItemDrop(PlayerDropItemEvent e){
+        if(Objects.requireNonNull(e.getPlayer().getInventory().getItem(8)).getType().equals(e.getItemDrop().getItemStack().getType())){
+            e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onBlockFall(EntityChangeBlockEvent event) {
+        if (event.getEntity() instanceof FallingBlock) {
+            event.setCancelled(true);
         }
     }
 }

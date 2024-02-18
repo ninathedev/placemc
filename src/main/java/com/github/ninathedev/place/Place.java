4f5af80ae0aa2e5396fb81d948b135bfd17f8146
ninathedev <realguybackup@gmail.com>
2024-02-18 16:59:50 +0800

    Fixed some bugs

--- src/main/java/com/github/ninathedev/place/Place.java
+++ src/main/java/com/github/ninathedev/place/Place.java
@@ -1,35 +1,38 @@
 package com.github.ninathedev.place;
 
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.FallingBlock;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityChangeBlockEvent;
-import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.player.PlayerBucketEmptyEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-import java.util.UUID;
+import java.util.*;
 
 import static com.github.ninathedev.place.Globals.*;
 
 public final class Place extends JavaPlugin implements Listener {
-    private Map<UUID, BukkitRunnable> playerBreakTimers = new HashMap<>();
-    private Map<UUID, BukkitRunnable> playerPlaceTimers = new HashMap<>();
+    private Map<UUID, Long> playerBreakTimers = new HashMap<>();
+    private Map<UUID, Long> playerPlaceTimers = new HashMap<>();
 
     public void addPlayerBreakTimer(Player player, long delayInSeconds) {
+        // Calculate the end time
+        long endTime = System.currentTimeMillis() + delayInSeconds * 1000;
 
         // Create a new timer
         BukkitRunnable runnable = new BukkitRunnable() {
@@ -43,9 +46,11 @@ public final class Place extends JavaPlugin implements Listener {
         runnable.runTaskLater(this, delayInSeconds * 20); // 20 ticks = 1 second
 
         // Store the timer
-        playerBreakTimers.put(player.getUniqueId(), runnable);
+        playerBreakTimers.put(player.getUniqueId(), endTime);
     }
     public void addPlayerPlaceTimer(Player player, long delayInSeconds) {
+        // Calculate the end time
+        long endTime = System.currentTimeMillis() + delayInSeconds * 1000;
 
         // Create a new timer
         BukkitRunnable runnable = new BukkitRunnable() {
@@ -58,9 +63,27 @@ public final class Place extends JavaPlugin implements Listener {
         // Start the timer
         runnable.runTaskLater(this, delayInSeconds * 20); // 20 ticks = 1 second
 
-        // Store the timer
-        playerPlaceTimers.put(player.getUniqueId(), runnable);
+        // Store the end time
+        playerPlaceTimers.put(player.getUniqueId(), endTime);
     }
+
+    public long getPlaceTimeLeft(Player player) {
+        if (playerPlaceTimers.containsKey(player.getUniqueId())) {
+            // Calculate the time left in seconds
+            long timeLeft = (playerPlaceTimers.get(player.getUniqueId()) - System.currentTimeMillis()) / 1000;
+            return timeLeft > 0 ? timeLeft : 0;
+        }
+        return 0; // Return 0 if there's no timer for the player
+    }
+    public long getBreakTimeLeft(Player player) {
+        if (playerBreakTimers.containsKey(player.getUniqueId())) {
+            // Calculate the time left in seconds
+            long timeLeft = (playerBreakTimers.get(player.getUniqueId()) - System.currentTimeMillis()) / 1000;
+            return timeLeft > 0 ? timeLeft : 0;
+        }
+        return 0; // Return 0 if there's no timer for the player
+    }
+
     @Override
     public void onEnable() {
         this.saveDefaultConfig();
@@ -111,9 +134,14 @@ public final class Place extends JavaPlugin implements Listener {
         }
     }
 
-    public void onEntityDamage(EntityDamageEvent e) {
-        if (e.getEntityType() == EntityType.ENDER_CRYSTAL) {
-            e.setCancelled(true);
+    @EventHandler
+    public void onEntityExplode(EntityExplodeEvent event) {
+        List<Entity> entities = event.getEntity().getNearbyEntities(0, 0, 0);
+        for (Entity entity : entities) {
+            if (entity.getType() == EntityType.ENDER_CRYSTAL) {
+                event.setCancelled(true);
+                break;
+            }
         }
     }
 
@@ -128,9 +156,7 @@ public final class Place extends JavaPlugin implements Listener {
         }
 
         Material block = e.getBlockPlaced().getType();
-        if (block == Material.LAVA
-                || block ==  Material.WATER
-                || block == Material.PISTON
+        if (block == Material.PISTON
                 || block == Material.STICKY_PISTON
                 || block == Material.FIRE
                 || block == Material.SOUL_FIRE
@@ -148,28 +174,37 @@ public final class Place extends JavaPlugin implements Listener {
         }
 
         if (playerPlaceTimers.containsKey(e.getPlayer().getUniqueId())) {
-            e.getPlayer().sendMessage("You have a "+getConfig().getInt("timers.place")+" second timer! If you think it has already been surpassed, it is either that the server is lagging or the game is bugged.");
+            e.getPlayer().sendMessage("You have "+getPlaceTimeLeft(e.getPlayer())+" second(s) left!");
             e.setCancelled(true);
             return;
         }
 
         addPlayerPlaceTimer(e.getPlayer(), getConfig().getInt("timers.place"));
     }
-
-    public void onBlockBreak(BlockBreakEvent e) {
-        if (!getBreakOnly()) e.setCancelled(true);
-        Material block = e.getBlock().getType();
-        if (block == Material.BARRIER || block == Material.BEDROCK){
-            e.getPlayer().sendMessage("You can't escape.");
+    @EventHandler
+    public void onBucketEmpty(PlayerBucketEmptyEvent e) {
+        Material bucket = e.getBucket();
+        if (bucket == Material.LAVA_BUCKET || bucket == Material.WATER_BUCKET) {
+            e.getPlayer().sendMessage("To prevent griefing and potentially breaking the map, we disabled that block.");
             e.setCancelled(true);
-            return;
         }
-        if (playerBreakTimers.containsKey(e.getPlayer().getUniqueId())) {
-            e.getPlayer().sendMessage("You have a "+getConfig().getInt("timers.break")+" second timer! If you think it has already been surpassed, it is either that the server is lagging or the game is bugged.");
-            e.setCancelled(true);
-            return;
-        }
+    }
+    public void onPlayerInteract(PlayerInteractEvent e) {
+        if (e.getAction() == Action.LEFT_CLICK_BLOCK && e.getPlayer().getGameMode() == GameMode.CREATIVE) {
+            if (!getBreakOnly()) e.setCancelled(true);
+            Material block = Objects.requireNonNull(e.getClickedBlock()).getType();
+            if (block == Material.BARRIER || block == Material.BEDROCK) {
+                e.getPlayer().sendMessage("You can't escape.");
+                e.setCancelled(true);
+                return;
+            }
+            if (playerBreakTimers.containsKey(e.getPlayer().getUniqueId())) {
+                e.getPlayer().sendMessage("You have "+getBreakTimeLeft(e.getPlayer())+" second(s) left!");
+                e.setCancelled(true);
+                return;
+            }
 
-        addPlayerBreakTimer(e.getPlayer(), getConfig().getInt("timers.break"));
+            addPlayerBreakTimer(e.getPlayer(), getConfig().getInt("timers.break"));
+        }
     }
 }
